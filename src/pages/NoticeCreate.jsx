06ba7ffc875947ed756dbc35b8718909a82a20b6
rntkatn01 <rntkatn01@outlook.com>
2023-01-04 19:03:08 -0500

    added Home > Notice to notice create

--- src/pages/NoticeCreate.jsx
+++ src/pages/NoticeCreate.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Tooltip from '../components/Tooltip';
 
 function NoticeCreate() {
@@ -6,6 +7,29 @@ function NoticeCreate() {
     <>
       {/* Post 3 */}
       <div className="bg-white shadow-md rounded border border-slate-200 p-5">
+        <div className="mb-4">
+          <ul className="inline-flex flex-wrap text-sm font-medium">
+            <li className="flex items-center">
+              <Link className="text-slate-500 hover:text-indigo-500" to="/">
+                Home
+              </Link>
+              <svg
+                className="h-4 w-4 fill-current text-slate-400 mx-2"
+                viewBox="0 0 16 16"
+              >
+                <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
+              </svg>
+            </li>
+            <li className="flex items-center">
+              <Link
+                className="text-slate-500 hover:text-indigo-500"
+                to="/notice/list"
+              >
+                Notice
+              </Link>
+            </li>
+          </ul>
+        </div>
         {/* Header */}
         <header className="flex justify-between items-start space-x-3 mb-4">
           {/* User */}
