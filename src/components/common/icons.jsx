ec3599b13359ad0612f0bfdbadea2f8a1dad1e54
jeeyool <jmin16@myseneca.ca>
2023-01-08 19:19:40 -0500

    Added DownloadIcon

--- src/components/common/icons.jsx
+++ src/components/common/icons.jsx
@@ -42,3 +42,11 @@ export function CloseIcon() {
     </svg>
   );
 }
+
+export function DownloadIcon() {
+  return (
+    <svg className="w-12 h-12 fill-current" viewBox="3 2 32 32">
+      <path d="M16 20c.3 0 .5-.1.7-.3l5.7-5.7-1.4-1.4-4 4V8h-2v8.6l-4-4L9.6 14l5.7 5.7c.2.2.4.3.7.3zM9 22h14v2H9z" />
+    </svg>
+  );
+}
