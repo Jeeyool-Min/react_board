a07eae44789f60f3da1a87f3d81e5fe7e73d716f
Min Young Choe <mchoe@myseneca.ca>
2023-01-11 15:55:41 -0500

    List updated

--- src/pages/NoticeList.jsx
+++ src/pages/NoticeList.jsx
@@ -6,7 +6,7 @@ import DeleteButton from '../components/DeleteButton';
 import PaginationNumeric from '../components/PaginationNumeric';
 import DateSelect from '../components/DateSelect';
 import FilterButton from '../components/DropdownFilter';
-import InvoicesTable from '../components/InvoicesTable';
+import InvoicesTable from '../components/NoticesTable';
 
 // import EditMenu from '../components/DropdownEditMenu';
 
@@ -51,27 +51,29 @@ function NoticeList() {
             {/* Left: Title */}
             <div className="mb-4 sm:mb-0">
               <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">
-                List ✨
+                Notice List ✨
               </h1>
             </div>
 
             {/* Right: Actions */}
             <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
               {/* Search form */}
-              <SearchForm placeholder="Search by invoice ID…" />
+              <SearchForm placeholder="Search by notice ID…" />
               {/* Create invoice button */}
-              <button
-                type="button"
-                className="btn bg-indigo-500 hover:bg-indigo-600 text-white"
-              >
-                <svg
-                  className="w-4 h-4 fill-current opacity-50 shrink-0"
-                  viewBox="0 0 16 16"
+              <Link to="/notice/create">
+                <button
+                  type="button"
+                  className="btn bg-indigo-500 hover:bg-indigo-600 text-white"
                 >
-                  <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                </svg>
-                <span className="hidden xs:block ml-2">Create Invoice</span>
-              </button>
+                  <svg
+                    className="w-4 h-4 fill-current opacity-50 shrink-0"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                  </svg>
+                  <span className="hidden xs:block ml-2">Create Notice</span>
+                </button>
+              </Link>
             </div>
           </div>
 
@@ -87,7 +89,7 @@ function NoticeList() {
                   >
                     All
                     {' '}
-                    <span className="ml-1 text-indigo-200">67</span>
+                    <span className="ml-1 text-indigo-200"> </span>
                   </button>
                 </li>
                 <li className="m-1">
@@ -95,9 +97,9 @@ function NoticeList() {
                     type="button"
                     className="inline-flex items-center justify-center text-sm font-medium leading-5 rounded-full px-3 py-1 border border-slate-200 hover:border-slate-300 shadow-sm bg-white text-slate-500 duration-150 ease-in-out"
                   >
-                    Paid
+                    Public
                     {' '}
-                    <span className="ml-1 text-slate-400">14</span>
+                    <span className="ml-1 text-slate-400"> </span>
                   </button>
                 </li>
                 <li className="m-1">
@@ -105,19 +107,9 @@ function NoticeList() {
                     type="button"
                     className="inline-flex items-center justify-center text-sm font-medium leading-5 rounded-full px-3 py-1 border border-slate-200 hover:border-slate-300 shadow-sm bg-white text-slate-500 duration-150 ease-in-out"
                   >
-                    Due
+                    Private
                     {' '}
-                    <span className="ml-1 text-slate-400">34</span>
-                  </button>
-                </li>
-                <li className="m-1">
-                  <button
-                    type="button"
-                    className="inline-flex items-center justify-center text-sm font-medium leading-5 rounded-full px-3 py-1 border border-slate-200 hover:border-slate-300 shadow-sm bg-white text-slate-500 duration-150 ease-in-out"
-                  >
-                    Overdue
-                    {' '}
-                    <span className="ml-1 text-slate-400">19</span>
+                    <span className="ml-1 text-slate-400" />
                   </button>
                 </li>
               </ul>
